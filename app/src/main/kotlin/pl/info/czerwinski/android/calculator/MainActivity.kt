f9e3f36803cbcde9ad79a2fb66e3f0daff11f1b0
Sławomir Czerwiński <slawomir@czerwinski.info.pl>
2016-04-24 21:11:04 +0200

    Added first test

--- app/src/main/kotlin/pl/info/czerwinski/android/calculator/MainActivity.kt
+++ app/src/main/kotlin/pl/info/czerwinski/android/calculator/MainActivity.kt
@@ -1,9 +1,12 @@
 package pl.info.czerwinski.android.calculator
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+	val textView : TextView by lazy { findViewById(R.id.text) as TextView }
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
---
+++ app/src/test/kotlin/pl/info/czerwinski/android/calculator/MainActivityTest.kt
@@ -0,0 +1,21 @@
+package pl.info.czerwinski.android.calculator
+
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricGradleTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricGradleTestRunner::class)
+@Config(constants = BuildConfig::class, sdk = intArrayOf(21))
+class MainActivityTest {
+
+	val activity : MainActivity by lazy { Robolectric.setupActivity(MainActivity::class.java) }
+
+	@Test
+	fun activityShouldBeCreated() {
+		assertNotNull(activity)
+		assertNotNull(activity.textView)
+	}
+}
