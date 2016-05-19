161b5f263b909e467a4f8dec1c4391187b13c47c
Sławomir Czerwiński <slawomir@czerwinski.info.pl>
2016-05-19 21:03:15 +0200

    Added more tests for Processor

--- app/src/test/kotlin/pl/info/czerwinski/android/calculator/processor/ProcessorTest.kt
+++ app/src/test/kotlin/pl/info/czerwinski/android/calculator/processor/ProcessorTest.kt
@@ -2,10 +2,27 @@ package pl.info.czerwinski.android.calculator.processor
 
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 import pl.info.czerwinski.android.calculator.widget.Key
 import kotlin.test.expect
 
-class ProcessorToStringTest {
+@RunWith(Parameterized::class)
+class ProcessorOutputTest(val keys: List<Key>, val expectedOutput: String) {
+
+	companion object {
+		@JvmStatic
+		@Parameterized.Parameters(name = "{0}: {1}")
+		fun data() = listOf(
+				arrayOf(listOf(Key.NUM_1, Key.NUM_2, Key.DECIMAL_POINT, Key.NUM_5), "12.5"),
+				arrayOf(listOf(Key.NUM_1, Key.NUM_2, Key.DECIMAL_POINT, Key.NUM_5, Key.PLUS, Key.NUM_5, Key.NUM_2, Key.DIV, Key.NUM_2), "12.5+52\u00F72"),
+				arrayOf(listOf(Key.NUM_1, Key.NUM_2, Key.DECIMAL_POINT, Key.NUM_5, Key.BACK), "12."),
+				arrayOf(listOf(Key.BACK), "0"),
+				arrayOf(listOf(Key.NUM_1, Key.NUM_2, Key.DECIMAL_POINT, Key.NUM_5, Key.CLEAR), "0"),
+				arrayOf(listOf(Key.NUM_2, Key.PLUS, Key.NUM_2, Key.EXEC), "4"),
+				arrayOf(listOf<Key>(), "0")
+		)
+	}
 
 	@Before
 	fun clearProcessor() {
@@ -13,18 +30,8 @@ class ProcessorToStringTest {
 	}
 
 	@Test
-	fun testSingleNumber() {
-		listOf(Key.NUM_1, Key.NUM_2, Key.DECIMAL_POINT, Key.NUM_5)
-				.map { it -> it.operation }
-				.map { it.push() }
-		expect("12.5") { Processor.toString() }
-	}
-
-	@Test
-	fun testMultipleNumbers() {
-		listOf(Key.NUM_1, Key.NUM_2, Key.DECIMAL_POINT, Key.NUM_5, Key.PLUS, Key.NUM_5, Key.NUM_2, Key.DIV, Key.NUM_2)
-				.map { it -> it.operation }
-				.map { it.push() }
-		expect("12.5+52\u00F72") { Processor.toString() }
+	fun testOutput() {
+		keys.map { it.operation.push() }
+		expect(expectedOutput) { Processor.toString() }
 	}
 }
